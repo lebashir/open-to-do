feca92eb2e88cf96b2dbf356e1a5c66de60fc594
lebashir <bashir.abihabib@gmail.com>
2017-07-30 15:51:00 -0500

    cleaned up and started working on api controller

--- Gemfile
+++ Gemfile
@@ -18,7 +18,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
-gem 'bootstrap-sass'
 gem 'figaro'
 gem 'active_model_serializers', '~> 0.10.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
-
-  root 'welcome#index'
-
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
