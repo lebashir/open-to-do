ca13ff4f005609bd2f71c2b7220dfde0ab160f6d
lebashir <bashir.abihabib@gmail.com>
2017-07-30 16:05:18 -0500

    completed authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::UsersController < ApplicationController
+
+  before_action :authenticated?
+
+  def index
+
+    users = User.all
+
+    render json: users, each_serializer: UserSerializer
+  end
 end
