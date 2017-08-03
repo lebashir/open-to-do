6d5007da562d977da862402b6054960f89c595ac
lebashir <bashir.abihabib@gmail.com>
2017-08-03 17:15:07 -0500

    completed destroying

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -18,6 +18,17 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def destroy
+    begin
+      user = User.find(params[:id])
+      user.destroy
+
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
+
   private
 
   def user_params
